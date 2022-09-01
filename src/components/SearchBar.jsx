9bc0327d61a26b209bc0e7fe039ca23e276eb9a3
Mohit D <mohit.dhaker04@gmail.com>
2022-09-01 22:22:06 +0530

    initial commit

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Paper, IconButton } from '@mui/material';
+import SearchIcon from '@mui/icons-material/Search';
+import KeyboardVoiceIcon from '@mui/icons-material/KeyboardVoice';
+
+const SearchBar = () => {
+    return (
+
+        <Paper component='form'
+            onSubmit={() => { }}
+            sx={{ borderRadius: 20, border: '1px solid #e3e3e3', pl: 2, boxShadow: 'none', mr: { sm: 5 } }}>
+
+            <input className='search-bar' placeholder='Search...' value='' onChange={() => { }} />
+
+            <IconButton type="submit" sx={{ p: '10px', color: 'black' }} >
+                <SearchIcon />
+                <KeyboardVoiceIcon />
+            </IconButton>
+
+        </Paper>
+    )
+}
+
+export default SearchBar
